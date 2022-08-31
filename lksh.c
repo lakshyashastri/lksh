e029736f59e8f918d5b405a58a283affa52d451c
Lakshya Shastri <lakshyashastri11@gmail.com>
2022-08-31 16:41:55 +0530

    ; support

--- lksh.c
+++ lksh.c
@@ -51,53 +51,68 @@ int main() {
         }
 
         // shell prompt
-        // printf("%s<%s%s@%s%s:%s%s>%s ", COLOR_GREEN, hostname, COLOR_RED, COLOR_BLUE, unameInfo.sysname, COLOR_PURPLE, CWD, COLOR_RESET);
         printf("%s<%s%s@%s%s:%s%s>%s ", COLOR_GREEN, hostname, COLOR_RED, COLOR_BLUE, unameInfo.sysname, COLOR_PURPLE, CWD, COLOR_RESET);
         input_length = getline(&input, &MAX_INPUT_LENGTH, stdin);
 
-        // parse input
-        char *sep = " \t";
-        char *token;
-        token = strtok(input, sep);
+        // parse ;
+        char *semi = ";";
+        char *cmds;
+        cmds = strtok(input, semi);
 
-        // convert tokenized string to array
-        char *splits[MAX_LENGTH];
-        int split_count = 0;
-        while (token != NULL) {
-            splits[split_count++] = token;
-            token = strtok(NULL, sep);
+        // convert input to input.split(';')
+        char *input_splitted[MAX_LENGTH];
+        int input_split_count = 0;
+        while (cmds != NULL) {
+            input_splitted[input_split_count++] = cmds;
+            cmds = strtok(NULL, semi);
         }
 
-        // remove trailing "\n" from last element in splits array
-        strtok(splits[split_count - 1], "\n");
+        for (int ii = 0; ii < input_split_count; ii++) {
 
-        // empty enter
-        if (strcmp(splits[0], "\n") == 0) {
-            continue;
-        }
+            // parse input for space
+            char *sep = " \t";
+            char *token;
+            token = strtok(input_splitted[ii], sep);
 
-        // check if command is valid
-        int valid = 0;
-        for (int i = 0; i < NUM_CMDS; i++) {
-            if (strcmp(splits[0], CMDS[i]) == 0) {
-                valid = 1;
-                break;
+            // convert tokenized string to array
+            char *splits[MAX_LENGTH];
+            int split_count = 0;
+            while (token != NULL) {
+                splits[split_count++] = token;
+                token = strtok(NULL, sep);
             }
-        }
-        
-        // if not valid
-        if (!valid) {
-            printf("%s is not a valid command\n", splits[0]);
-            continue;
-        }
 
-        // execute commands
-        if (strcmp(splits[0], "pwd") == 0) {
-            lksh_pwd();
-        } else if (strcmp(splits[0], "echo") == 0) {
-            lksh_echo(splits, split_count);
-        } else if (strcmp(splits[0], "cd") == 0) {
-            lksh_cd(splits, split_count);
+            // remove trailing "\n" from last element in splits array
+            strtok(splits[split_count - 1], "\n");
+
+            // empty enter
+            if (strcmp(splits[0], "\n") == 0) {
+                continue;
+            }
+
+            // check if command is valid
+            int valid = 0;
+            for (int i = 0; i < NUM_CMDS; i++) {
+                if (strcmp(splits[0], CMDS[i]) == 0) {
+                    valid = 1;
+                    break;
+                }
+            }
+            
+            // if not valid
+            if (!valid) {
+                printf("%s is not a valid command\n", splits[0]);
+                continue;
+            }
+
+            // execute commands
+            if (strcmp(splits[0], "pwd") == 0) {
+                lksh_pwd();
+            } else if (strcmp(splits[0], "echo") == 0) {
+                lksh_echo(splits, split_count);
+            } else if (strcmp(splits[0], "cd") == 0) {
+                lksh_cd(splits, split_count);
+            }
         }
     }
 }
---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	gcc -o lksh lksh.c
